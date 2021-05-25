d6133cb5bc885b792856c6bbff8ab34aecd6fb20
blusanders <blusanders@yahoo.com>
2021-05-25 10:23:23 -0500

    Redoing queries

--- 17 - invoices_line_item_count.sql
+++ 17 - invoices_line_item_count.sql
@@ -1,3 +1,12 @@
-select Invoice.*, InvoiceLine.*
-from Invoice
-join InvoiceLine on invoice.InvoiceId = InvoiceLine.InvoiceId
+select
+    Invoice.*, count(InvoiceLine.InvoiceId)
+
+from
+    Invoice
+
+join InvoiceLine
+    on invoice.InvoiceId = InvoiceLine.InvoiceId
+
+group by
+    invoice.InvoiceId
+
--- 19 - top_2009_agent.sql
+++ 19 - top_2009_agent.sql
@@ -1,22 +1,26 @@
 select
 
+    EmpName,
     max(totalSales)
 
 from 
 
-(
+    (
 
-select
+        select
 
-sum(invoice.Total) as totalSales,
-Employee.FirstName || " " || Employee.LastName as "EmpName"
+        sum(invoice.Total) as totalSales,
+        Employee.FirstName || " " || Employee.LastName as "EmpName"
 
-from Employee
+        from Employee
 
-left join Customer on Customer.SupportRepId = employee.EmployeeId
-left join invoice on invoice.CustomerId = customer.CustomerId
+        left join Customer on Customer.SupportRepId = employee.EmployeeId
+        left join invoice on invoice.CustomerId = customer.CustomerId
 
-group by employee.EmployeeId 
-order by totalSales desc
+        where invoice.InvoiceDate like "%2009%"
 
-)
+        group by employee.EmployeeId 
+        order by totalSales desc
+
+
+    )
